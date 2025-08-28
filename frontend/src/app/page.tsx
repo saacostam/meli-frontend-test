9055ce74d69ad551e87dcaa699c95622f54c3155
saacostam <73088662+saacostam@users.noreply.github.com>
2025-08-28 18:08:37 -0500

    style(formatter)

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Skeleton } from "@/client/core.components";
 
 export default function Home() {
-  return <div className="bg-white rounded p-4 space-y-4">
-    <Skeleton className="w-full h-48"/>
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-      <Skeleton className="w-full h-48"/>
-      <Skeleton className="w-full h-48"/>
-      <Skeleton className="w-full h-48"/>
-      <Skeleton className="w-full h-48"/>
+  return (
+    <div className="bg-white rounded p-4 space-y-4">
+      <Skeleton className="w-full h-48" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+        <Skeleton className="w-full h-48" />
+        <Skeleton className="w-full h-48" />
+        <Skeleton className="w-full h-48" />
+        <Skeleton className="w-full h-48" />
+      </div>
+      <Skeleton className="w-full h-48" />
     </div>
-    <Skeleton className="w-full h-48"/>
-  </div>;
+  );
 }
--- frontend/src/client/search-results/components/search-results.tsx
+++ frontend/src/client/search-results/components/search-results.tsx
@@ -16,9 +16,11 @@ export function SearchResults({ items, breadCrumbs }: SearchResultsProps) {
 
   return (
     <>
-      {!isEmpty && <div className="mb-4">
-        <BreadCrumbs data={breadCrumbs} />
-      </div>}
+      {!isEmpty && (
+        <div className="mb-4">
+          <BreadCrumbs data={breadCrumbs} />
+        </div>
+      )}
       <div className="bg-white divide-y divide-gray-200 rounded">
         {isEmpty && (
           <div className="p-4">
