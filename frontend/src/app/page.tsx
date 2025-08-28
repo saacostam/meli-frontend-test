ff5cc24625066109efda8426ab26680d90fe759a
saacostam <73088662+saacostam@users.noreply.github.com>
2025-08-28 17:57:48 -0500

    feat: add skeleton-based home

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Skeleton } from "@/client/core.components";
+
 export default function Home() {
-  return null;
+  return <div className="bg-white rounded p-4 space-y-4">
+    <Skeleton className="w-full h-48"/>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+      <Skeleton className="w-full h-48"/>
+      <Skeleton className="w-full h-48"/>
+      <Skeleton className="w-full h-48"/>
+      <Skeleton className="w-full h-48"/>
+    </div>
+    <Skeleton className="w-full h-48"/>
+  </div>;
 }
--- frontend/src/client/core.components/skeleton/skeleton.tsx
+++ frontend/src/client/core.components/skeleton/skeleton.tsx
@@ -7,7 +7,7 @@ export interface SkeletonProps {
 export function Skeleton({ className }: SkeletonProps) {
   return (
     <div
-      className={twMerge("bg-gray-100 animate-pulse rounded-2xl", className)}
+      className={twMerge("bg-gray-200 animate-pulse rounded-2xl", className)}
     />
   );
 }
