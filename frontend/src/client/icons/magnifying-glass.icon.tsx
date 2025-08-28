de10fb63075000606e51a1d86c6f93fc2ad2a939
saacostam <73088662+saacostam@users.noreply.github.com>
2025-08-28 03:05:07 -0500

    feat: add icons module

---
+++ frontend/src/client/icons/magnifying-glass.icon.tsx
@@ -0,0 +1,25 @@
+import { twMerge } from "tailwind-merge";
+
+export interface MagnifyingGlassIconProps {
+  className?: string;
+}
+
+export function MagnifyingGlassIcon({ className }: MagnifyingGlassIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={2}
+      stroke="currentColor"
+      className={twMerge("size-6", className)}
+    >
+      <title>Magnyfying Glass Icon</title>
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+      />
+    </svg>
+  );
+}
