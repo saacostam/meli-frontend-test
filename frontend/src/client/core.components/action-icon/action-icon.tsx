9caf28a1d3ea2ffdbc0f060a0572c147d3e2c756
saacostam <73088662+saacostam@users.noreply.github.com>
2025-08-28 04:49:20 -0500

    style(formatter)

--- frontend/src/client/core.components/action-icon/action-icon.tsx
+++ frontend/src/client/core.components/action-icon/action-icon.tsx
@@ -6,7 +6,10 @@ export function ActionIcon({ className, type, ...rest }: ActionIconProps) {
   return (
     <button
       {...rest}
-      className={twMerge("bg-gray-200 p-2 text-gray-500 focus:outline outline-blue-400 hover:cursor-pointer", className)}
+      className={twMerge(
+        "bg-gray-200 p-2 text-gray-500 focus:outline outline-blue-400 hover:cursor-pointer",
+        className,
+      )}
       //@ts-ignore
       type={type || "button"}
     />
