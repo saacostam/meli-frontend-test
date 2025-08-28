303d72c46d6ec410fb39705878478df89051b0d1
saacostam <73088662+saacostam@users.noreply.github.com>
2025-08-28 17:44:48 -0500

    refactor: simplify breadcrumbs component

--- frontend/src/client/core.components/breadcrumbs/breadcrumbs.tsx
+++ frontend/src/client/core.components/breadcrumbs/breadcrumbs.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { useMemo } from "react";
 import { ChevronRightIcon } from "@/client/icons";
 import type { TBreadCrumb } from "./breadcrumbs.types";
 
@@ -8,42 +7,24 @@ export interface BreadCrumbsProps {
 }
 
 export function BreadCrumbs({ data }: BreadCrumbsProps) {
-  const breadCrumbsWithChevrons = useMemo(() => {
-    const breadCrumbsWithChevrons: (
-      | { type: "breadcrumb"; payload: TBreadCrumb }
-      | { type: "chevron" }
-    )[] = [];
-
-    const n = data.length;
-
-    for (let i = 0; i < n; i++) {
-      breadCrumbsWithChevrons.push({
-        type: "breadcrumb",
-        payload: data[i],
-      });
-      if (i < n - 1) breadCrumbsWithChevrons.push({ type: "chevron" });
-    }
-
-    return breadCrumbsWithChevrons;
-  }, [data]);
-
   return (
-    <div className="flex flex-wrap gap-2 text-gray-700 items-center text-sm">
-      {breadCrumbsWithChevrons.map((entry, index) => (
-        <span key={+index}>
-          {entry.type === "breadcrumb" ? (
-            entry.payload.href ? (
-              <Link href={entry.payload.href} prefetch={false}>
-                {entry.payload.label}
+    <nav aria-label="breadcrumb">
+      <ol className="flex flex-wrap gap-2 text-gray-700 items-center text-sm">
+        {data.map((crumb, index) => (
+          <li key={+index} className="flex items-center">
+            {crumb.href ? (
+              <Link href={crumb.href} prefetch={false}>
+                {crumb.label}
               </Link>
             ) : (
-              <span>{entry.payload.label}</span>
-            )
-          ) : (
-            <ChevronRightIcon className="size-3" />
-          )}
-        </span>
-      ))}
-    </div>
+              <span>{crumb.label}</span>
+            )}
+            {index < data.length - 1 && (
+              <ChevronRightIcon className="size-3 ml-2" />
+            )}
+          </li>
+        ))}
+      </ol>
+    </nav>
   );
 }
